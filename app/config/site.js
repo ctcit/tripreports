33e6d76f296c9fab06ecac836608035a21f412e5
Bruce James <bruce.james@clear.net.nz>
2016-03-05 14:48:24 +1300

    Keep site config in seperate file so it local customizations can be made and (using .gitignore) kept out of master Partially fixed tests (somewhere site.URL became site.url)

---
+++ app/config/site.js
@@ -0,0 +1,32 @@
+﻿﻿(function () {
+     'use strict';
+ 
+    // change as appropriate; comment out to set it from window.location
+     var site_url = 'http://www.ctc.org.nz';
+
+     if (!site_url) {
+         // Set global constant site.url from window.location
+         var full_url = window.location.href,
+             pathMatcher = new RegExp('(.*?)/tripreports.*'),
+             bits = pathMatcher.exec(full_url),
+             site_url = 'invalidurl';
+         if (bits != null) {
+             site_url = bits[1];
+         } else {
+             console.log("TripReport module fetched from an unexpected address");
+         }
+     }
+
+     console.log("Setting site url to " + site_url);
+
+     angular.module('tripReportApp').constant('site',
+         {
+             'url': site_url,
+             'tripreportbaseurl': site_url + '/index.php/trip-reports',
+             'resturl': site_url + '/db/index.php/rest',
+             'imageurl': site_url
+         });
+
+}());
+
+
--- tests/app/trip-edit-controller.tests.js
+++ tests/app/trip-edit-controller.tests.js
@@ -126,11 +126,11 @@ describe('TripEditController: ', function () {
             .whenGET(/app\/.*\.html/).respond(200, ''); // workaround for unexpected requests of views
 
         $httpBackend
-            .when('GET', site.URL + '/db/index.php/rest/user')
+            .when('GET', site.url + '/db/index.php/rest/user')
             .respond({ "id": 0 });
 
         $httpBackend
-            .when('GET', site.URL + '/db/index.php/rest/tripreports/' + tripDetails.id)
+            .when('GET', site.url + '/db/index.php/rest/tripreports/' + tripDetails.id)
             .respond(tripDetails);
 
         controller = createController(tripDetails.id);
