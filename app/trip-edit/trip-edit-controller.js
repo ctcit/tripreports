7fda1ad467451bcfaa48751190b7bd4a6ec7687c
Richard Lobb <richard.lobb@canterbury.ac.nz>
2017-10-30 21:11:25 +1300

    Allow selection of multiple images/gpx/map files for file upload.

--- app/trip-edit/trip-edit-controller.js
+++ app/trip-edit/trip-edit-controller.js
@@ -17,21 +17,21 @@
             $scope.deletedImages = [];
             $scope.deletedGpxs = [];
             $scope.submissionErrors = false;
-         
+
             $scope.loading = true;
             tripReportService.get({ 'tripId': id },
                 function (tripReport) {
                     currentTripReportService.currentTripReport = tripReport;
                     $scope.tripReport = tripReport;
                     // Set resource types in here so template processes resources now
-                    $scope.resourceTypes = ['image', 'gpx', 'map'];  
+                    $scope.resourceTypes = ['image', 'gpx', 'map'];
                 }, function(response) {
                     alert("Couldn't fetch trip report (" + response.status + "). A network problem?");
                 }).$promise.finally(function() {
                     $scope.loading = false;
                 });
-            
-        
+
+
             $scope.imageUrl = function(resourceType, imageNum, thumbReqd) {
                 var image;
                 if ($scope.tripReport[resourceType + 's'] === undefined) {
@@ -46,13 +46,13 @@
                         return site.imageurl + '/dbcaptionedimage.php?id=' + image.id;
                     }dbthumb
                 }
-            } 
+            }
 
             $scope.capitalise = function (word) {
                 // Return the word with first char in upper case
                 return word.charAt(0).toUpperCase() + word.substring(1);
             };
-            
+
             $scope.moveUp = function(resource, resourceNum) {
                 // Move up the given resouce. resource must be 'image',
                 // 'map' or 'gpx'
@@ -61,7 +61,7 @@
                 $scope.tripReport[field][resourceNum] = $scope.tripReport[field][resourceNum - 1];
                 $scope.tripReport[field][resourceNum - 1] = temp;
             };
-            
+
             $scope.moveDown = function(resource, resourceNum) {
                 // Move down the given resouce. resource must be 'image',
                 // 'map' or 'gpx'
@@ -70,7 +70,7 @@
                 $scope.tripReport[field][resourceNum] = $scope.tripReport[field][resourceNum + 1];
                 $scope.tripReport[field][resourceNum + 1] = temp;
             };
-            
+
             $scope.delete = function(resourceType, index) {
                 var resources = $scope.tripReport[resourceType + 's'],
                     id = 0,
@@ -85,10 +85,10 @@
                         }
                     }
                     resources.splice(index, 1);  // Delete that item
-                }    
+                }
             }
 
-            
+
             $scope.downsize = function(image) {
                 // Downsize the given image so its maximum dimension
                 // is MAX_UPLOAD_IMAGE_DIMENSION. When done, set
@@ -129,8 +129,8 @@
                 };
                 osImage.src = image.dataUrl;
             }
-            
-            
+
+
             $scope.addResource = function(resourceType, filename) {
                 // Adds a new resource (image, map or gpx file) to the trip report.
                 // At this stage the resource has not been uploaded so the
@@ -147,31 +147,40 @@
                 resources.push(file);
                 return file;
             }
-            
- 
+
+
             $scope.fileSelected = function(event, files, resourceType) {
-                // Called when a file input is selected. Adds the file to
+                // Called when a file input is selected. Adds the file(s) to
                 // the list of resources, but with a zero id. Then initiates
-                // converting of the file to a dataUrl (downsizing it if
-                // it's an image).
-                var reader = new FileReader(), 
-                    file = $scope.addResource(resourceType, files[0].name);
-                       
-                reader.onload = function() {
-                    $scope.$apply(function () {
-                        file.dataUrl = reader.result;
-                        if (resourceType !== 'gpx') { // Downsize maps and images
-                            $scope.downsize(file);
-                        } else {
-                            file.loaded = true;
-                        }
-                    });
-                };    
-                reader.readAsDataURL(files[0]);
+                // converting of the files to dataUrls (downsizing in the
+                // case of images).
+
+                function initiateRead(file) {
+                    var reader = new FileReader(),
+                        fileResource = $scope.addResource(resourceType, file.name);
+
+                    reader.readAsDataURL(file);
+
+                    reader.onload = function() {
+                        $scope.$apply(function () {
+                            fileResource.dataUrl = reader.result;
+                            if (resourceType !== 'gpx') { // Downsize maps and images
+                                $scope.downsize(fileResource);
+                            } else {
+                                fileResource.loaded = true;
+                            }
+                        });
+                    };
+                }
+
+                for (var i = 0; i < files.length; i++) {
+                    initiateRead(files[i]);
+                };
                 event.srcElement.value = '';  // Reset file input for another file
+
             };
-            
-            
+
+
             $scope.badDate = function() {
                 // Return true if a submitted form has an invalid date or duration
                 // Applicable only for trip reports, not news reports.
@@ -181,36 +190,36 @@
                          $scope.tripReport.duration == 0
                         );
             }
-            
+
             $scope.badSubmittedDate = function() {
                 // True if the date was bad while the form was being submitted
                 return $scope.submissionErrors && $scope.badDate();
             }
-            
+
             $scope.emptyBody = function() {
                 // Return true if a submitted form has an empty body
                 return $scope.submissionErrors && $scope.tripReport.body == '';
             }
-            
-            
+
+
             $scope.titleClass = function() {
                 // Return a css class of 'error' if the submitted trip's title
                 // is empty
                 return $scope.submissionErrors && $scope.tripReport.title == '' ? 'error' : '';
             };
-            
+
             $scope.submitOrSave = function() {
                 // Return a value for use in the submit/save button
                 // Deal with race problem (tripReport undefined) by checking
                 // both if the tripReport is defined then if it has a non-zero id.
                 return $scope.tripReport && $scope.tripReport.id ? 'Save changes' : 'Submit';
             };
-            
+
             $scope.getDateDisplay = function() {
                 // Return a date display string for the current trip report of
                 // the form 1-3 June 2014 or perhaps 30 April - 1 May 2014
                 var months = ['January', 'February', 'March', 'April', 'May',
-                       'June', 'July', 'August', 'September', 'October', 
+                       'June', 'July', 'August', 'September', 'October',
                        'November', 'December'],
                     yearStart = parseInt($scope.tripReport.year),
                     monthStart = $scope.tripReport.month - 1,  // 0-origin
@@ -222,20 +231,20 @@
                     monthEnd = endDate.getMonth(),
                     yearEnd = parseInt(endDate.getFullYear()),
                     result = '';
-            
+
                 if ($scope.badDate()) {
                     return 'Undefined date/duration';
                 }
-            
+
                 // Assume trips are less than 11 months!
                 if ($scope.tripReport.duration === 1) {
                     result = dayStart + ' ' + months[monthStart] + ' ' + yearStart;
                 }
                 else if (monthStart === monthEnd) { // Same month?
-                    result = (dayStart + '-' + dayEnd + ' ' + 
+                    result = (dayStart + '-' + dayEnd + ' ' +
                               months[monthStart] + ' ' + yearStart);
                 } else if (yearStart === yearEnd) {  // Change of month only
-                    result = (dayStart + ' ' + months[monthStart] + 
+                    result = (dayStart + ' ' + months[monthStart] +
                                 ' - ' + dayEnd + ' ' + months[monthEnd] +
                                 ' ' + yearEnd);
                 } else { // Trip crosses new-year boundary
@@ -245,7 +254,7 @@
                 }
                 return result;
             };
-            
+
             $scope.updateDateDisplay = function() {
                 // Called when user_set_date_display changes. If it's being
                 // turned on, initialise it to the date generated by getDateDisplay
@@ -253,29 +262,29 @@
                     $scope.tripReport.date_display = $scope.getDateDisplay();
                 }
             }
-            
-            
+
+
             $scope.submit = function() {
                 // Handle a click on the submit/save button
-                
+
                 function validate() {
                     return $scope.tripReport.title !== "" &&
                            !$scope.badDate() &&
                            $scope.tripReport.body !== ""
                 }
-                
+
                 function uploadFile(file, service) {
                     // Local function to upload an image or gpx file
                     // to the server using the given service (a resource in
                     // angular parlance). Return the promise of the upload.
                     var promise;
-                    response = service.save(                        
+                    response = service.save(
                         {name:      file.name,
                          caption:   file.caption,
                          dataUrl:   file.dataUrl
                         }
                     );
-                    promise = response.$promise.then(function(data)  { 
+                    promise = response.$promise.then(function(data)  {
                             file.id = data.id;
                         },
                         function(error) {
@@ -283,12 +292,12 @@
                         }
                     );
                     return promise;
-                }    
+                }
 
                 // First, send all new files (images and gpxs) to the server.
                 var i, gpx, image, imageId, gpxId, images, response, promises;
-                
-                
+
+
                 if (!validate()) {
                     $scope.submissionErrors = true;
                     alert("The trip report has errors. Please correct them and resubmit.");
@@ -304,17 +313,17 @@
                         promises.push(imageService.delete({imageId: imageId}));
                     }
                     $scope.deletedImages = [];  // Done, so clear the list
-                    
+
                     for (i = 0; i < $scope.deletedGpxs.length; i++) {
                         gpxId = $scope.deletedGpxs[i];
                         promises.push(gpxService.delete({gpxId: gpxId}));
                     }
                     $scope.deletedGpxs = [];
-                    
-                    // Now save (if new) or update 
+
+                    // Now save (if new) or update
                     // (otherwise) the actual trip report.
                     $q.all(promises).catch(function(response) {
-                        alert("Deletion of an image or gpx failed (" + 
+                        alert("Deletion of an image or gpx failed (" +
                                 response.status + "). Check the result carefully");
                     }).finally(function(result) {
                         $scope.saveOrUpdate().then(
@@ -323,16 +332,16 @@
                                     $state.go('tripreports.show', { tripId: result.id });
                                 },
                                 function(response) { // Fail
-                                    alert("Trip report upload failed with error " + 
+                                    alert("Trip report upload failed with error " +
                                             response.status + ' ' + response.data);
                                 }).finally(function() {
                                     $scope.saving = false;
                                 });
                     });
                 }
-                
+
             };
-            
+
             $scope.saveOrUpdate = function() {
                 if ($scope.tripReport.id === 0) {
                     return $scope.tripReport.$save();
@@ -340,7 +349,7 @@
                     return $scope.tripReport.$update();
                 }
             }
-            
+
             $scope.cancel = function() {
                 // Handle a click on the cancel button
                 if ($scope.confirm('Discard all your changes and go back to browsing trip reports. Are you QUITE SURE?!')) {
@@ -352,6 +361,6 @@
                 // Wrap so it can be easily mocked
                 return confirm(message);
             }
-            
+
         }]);
 }());
