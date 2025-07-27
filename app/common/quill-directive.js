60500cb662a6f47fc67df758c9d79ab7893aa1f7
Nick Edwards <nickedwrds@gmail.com>
2025-07-27 18:06:35 +1200

    Attempt even betterer quill->html ocnversion

--- app/common/quill-directive.js
+++ app/common/quill-directive.js
@@ -24,13 +24,22 @@ angular.module('tripReportApp').directive('quillEditor', function() {
             function convertToProperHTML() {
                 var html = quill.root.innerHTML;
                 
-                // Clean up empty paragraphs with just breaks or whitespace
+                // First, handle newline characters that Quill sometimes uses
+                html = html.replace(/\n/g, '</p><p>');
+                
+                // Handle &nbsp; sequences that represent line breaks
+                html = html.replace(/&nbsp;&nbsp;/g, '</p><p>');
+                html = html.replace(/(&nbsp;\s*){2,}/g, '</p><p>');
+                
+                // Clean up empty paragraphs with just breaks, whitespace, or &nbsp;
                 html = html.replace(/<p>\s*<br\s*\/?>\s*<\/p>/gi, '');
                 html = html.replace(/<p>\s*<\/p>/gi, '');
                 html = html.replace(/<p>\s*&nbsp;\s*<\/p>/gi, '');
+                html = html.replace(/<p>(&nbsp;|\s)*<\/p>/gi, '');
                 
-                // Handle paragraphs that end with <br> tags
+                // Handle paragraphs that end with <br> tags or &nbsp;
                 html = html.replace(/<br\s*\/?>\s*<\/p>/gi, '</p>');
+                html = html.replace(/&nbsp;\s*<\/p>/gi, '</p>');
                 
                 // Handle multiple consecutive <br> tags and convert them to paragraph breaks
                 html = html.replace(/<br\s*\/?>\s*<br\s*\/?>/gi, '</p><p>');
@@ -38,9 +47,17 @@ angular.module('tripReportApp').directive('quillEditor', function() {
                 // Handle single <br> tags that should be paragraph breaks (when between content)
                 html = html.replace(/([^>])\s*<br\s*\/?>\s*([^<])/gi, '$1</p><p>$2');
                 
+                // Handle &nbsp; sequences between content that should be paragraph breaks
+                html = html.replace(/([^>])\s*&nbsp;\s*&nbsp;\s*([^<])/gi, '$1</p><p>$2');
+                html = html.replace(/([^>])\s*&nbsp;\s*([^<])/gi, '$1 $2'); // Single &nbsp; becomes space
+                
                 // Clean up any remaining isolated <br> tags at the start/end of paragraphs
                 html = html.replace(/<p>\s*<br\s*\/?>/gi, '<p>');
                 html = html.replace(/<br\s*\/?>\s*<\/p>/gi, '</p>');
+                
+                // Clean up &nbsp; at start/end of paragraphs
+                html = html.replace(/<p>\s*&nbsp;/gi, '<p>');
+                html = html.replace(/&nbsp;\s*<\/p>/gi, '</p>');
                 
                 // Handle text that's not wrapped in paragraphs
                 if (html && !html.trim().startsWith('<')) {
@@ -56,6 +73,9 @@ angular.module('tripReportApp').directive('quillEditor', function() {
                 // Remove completely empty paragraphs at start/end
                 html = html.replace(/^(\s*<p>\s*<\/p>\s*)+/gi, '');
                 html = html.replace(/(\s*<p>\s*<\/p>\s*)+$/gi, '');
+                
+                // Final cleanup - remove any lingering empty paragraphs with whitespace/nbsp
+                html = html.replace(/<p>[\s&nbsp;]*<\/p>/gi, '');
                 
                 return html.trim();
             }
